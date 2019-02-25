ea54bb184d2d0d22ed363ffa85bb8b7e0bc9bdc9
Alex R <alexander.m.rankin@gmail.com>
2019-02-24 21:55:37 -0500

    basic site layout

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 
 def main(request):
     user = request.session.get('user', None)
@@ -8,7 +9,13 @@ def main(request):
     if user is not None and password is not None:
         user = authenticate(username=user, password=password)
         if user is not None:
-            return HttpResponse("dashboard page!")
+            user_object = User.objects.get(username=user)
+            context = {
+                'username':user,
+                'is_super':user_object.is_superuser,
+            }
+
+            return render(request, 'dashboard/base.html', context)
         else:
             return redirect('login-main')
     else:
