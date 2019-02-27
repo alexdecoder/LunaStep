5bc1edb20935d37cfa0966318a15645da7300bbf
Alex R <alexander.m.rankin@gmail.com>
2019-02-26 22:00:39 -0500

    super perms begin

--- dashboard/static/dashboard/base.js
+++ dashboard/static/dashboard/base.js
@@ -13,4 +13,29 @@ window.addEventListener("load", () =>
         searchWrapper.style.backgroundColor = "#32325d"
         searchWrapper.style.boxShadow = "none" 
     })
+
+    var select = 0
+    var navbarLinks = document.getElementsByClassName("navbar-node")
+    for (let index = 0; index < navbarLinks.length; index++) {
+        const element = navbarLinks[index];
+        element.addEventListener("click", function()
+        {
+            navbarLinks[select].style.color = "#6b7c93"
+
+            switchPage(select, index)
+
+            select = index
+            element.style.color = "#43458b"
+        })
+    }
+    navbarLinks[select].style.color = "#43458b"
+    contentPages[select].style.display = "block"
 })
+
+var current = 0
+var contentPages = document.getElementsByClassName("content-section")
+function switchPage(from, index)
+{
+    contentPages[from].style.display = "none"
+    contentPages[index].style.display = "block"
+}
